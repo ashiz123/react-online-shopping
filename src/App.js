da7136af37eb9f24c862695e51e7228a3586e5e1
DESKTOP-PRBKQSV\ashiz <ashizh@khozinfo.com>
2020-04-19 16:26:41 +0100

    update our menu items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import './App.css';
+import {Switch, Route, Link} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+
+
+const HatsPage = () =>(
+  <div>
+    <h1>testing </h1>
+  </div>
+)
+
+const TopicDetail = (props) =>(
+  <div>
+    <h1>{props.match.params.topicId} </h1>
+    <Link to={`${props.match.url}/13`} />
+    <button onClick={() => props.history.goBack()}>Hats</button>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+     <Route exact path="/" component = {HomePage} />
+     <Route  path="/hats" component = {HatsPage} />
+     <Route  path="/topics/:topicId" component = {TopicDetail} />
+     <Route  path="/blog/topics/:topicId" component = {TopicDetail} />
+     </Switch>
     </div>
   );
 }
