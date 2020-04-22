45ef4da47744912aa0b788da7da496449ab5ec4b
DESKTOP-PRBKQSV\ashiz <ashizh@khozinfo.com>
2020-04-22 12:41:08 +0100

    cart added to item render created

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 
 import './cart-dropdown.styles.scss';
+import CartItem from '../cart-item/cart-item.component';
+import {connect} from 'react-redux';
 
 import CustomButton from '../custom-button/custom.button.component';
 
-const CartDropdown = () => (
+const mapStateToProps = ({cart: {cartItems}}) => 
+({
+    cartItems
+})
+
+const CartDropdown = ({cartItems}) => (
     <div className="cart-dropdown">
         <div className="cart-items">
-            <CustomButton type="button" >GO TO CHECKOUT</CustomButton>
+            {
+                cartItems.map((cartItem) =>
+                <CartItem key = {cartItem.id} item = {cartItem} />
+                )
+            }
+        
         </div>
+        <CustomButton type="button" >GO TO CHECKOUT</CustomButton>
     </div>
 )
 
-export default CartDropdown;
+export default connect(mapStateToProps)(CartDropdown);
