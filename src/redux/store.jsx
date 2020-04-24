060cf289d3ceb83f2413a51d4afbf509511a77e3
DESKTOP-PRBKQSV\ashiz <ashizh@khozinfo.com>
2020-04-24 22:41:47 +0100

    changes in store file

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -6,10 +6,21 @@ import rootReducer from './root-reducer';
 
 const middlewares = [logger]; //to do more middlewares.
 //...middlewares get the all the values and function in upper middleares array.
-export const store =  createStore(rootReducer, compose(
-    applyMiddleware(...middlewares),
-    window.devToolsExtension ? window.devToolsExtension() : f => f
-  )  );
+// export const store =  createStore(rootReducer, compose(
+//     applyMiddleware(...middlewares),
+//     window.devToolsExtension ? window.devToolsExtension() : f => f
+//   )  );
+
+export const store =  createStore(rootReducer, 
+  applyMiddleware(...middlewares)
+ 
+  );
+
+
+  // export const store =  createStore(rootReducer, compose(
+  //   applyMiddleware(...middlewares),
+  //   window.devToolsExtension ? window.devToolsExtension() : f => f
+  // )  );
 
 
   export const persistor = persistStore(store); //created persitsted version of store.
